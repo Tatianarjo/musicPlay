ab87db88d837a3d633ad9a03243f3f8505108b64
Tatiana Johnson <Tatianarjo@gmail.com>
2023-02-07 20:01:06 -0600

    Added song card placement

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,5 +1,6 @@
 // import { Error, Loader, Songcard } from "../components";
 import { genres } from "../assets/constants";
+import { SongCard } from "../components";
 
 const Discover = () => {
   console.log(genres);
@@ -20,9 +21,16 @@ const Discover = () => {
           className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
         >
           {genres.map((genre) => (
-            <option key={genre.value} value={genre.value}>{genre.title}</option>
+            <option key={genre.value} value={genre.value}>
+              {genre.title}
+            </option>
           ))}
         </select>
+      </div>
+      <div className="flex flex-wrap sm: justify-start justify-center gap-8">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+          <SongCard key={song.key} song={song} i={i} />
+        ))}
       </div>
     </div>
   );
