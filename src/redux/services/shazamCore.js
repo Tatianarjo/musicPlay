5dd9922dcbaa75895bbcb6e1675cca4f002ebde0
Tatiana Johnson <Tatianarjo@gmail.com>
2023-02-07 20:42:40 -0600

    added API information

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        ""
+      );
+      return headers;
+    },
+  }),
+});
