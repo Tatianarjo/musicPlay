a471a29afccaf1342c6150816ffd818ff61b7383
Tatiana Johnson <Tatianarjo@gmail.com>
2023-02-07 18:21:18 -0600

    first commit

---
+++ src/pages/Discover.jsx
@@ -0,0 +1,10 @@
+// import { Error, Loader, Songcard } from "../components";
+import { genres } from '../assets/constants';
+
+const Discover = () => {
+  console.log(genres);
+
+  return <div className="flex flex-col">Discover</div>;
+};
+
+export default Discover;
