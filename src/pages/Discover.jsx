49ef46e626bf6913eaa77cf74d11784a147e87db
Tatiana Johnson <Tatianarjo@gmail.com>
2023-02-08 21:33:14 -0600

    added loading and error state

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,13 +1,15 @@
-// import { Error, Loader, Songcard } from "../components";
-import { genres } from "../assets/constants";
-import { SongCard } from "../components";
-import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+import { Error, Loader, SongCard } from '../components';
+import { genres } from '../assets/constants';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const Discover = () => {
   const { data, isFetching, error } = useGetTopChartsQuery();
   console.log(genres);
   const genreTitle = "Pop";
-  console.log(data);
+
+  if (isFetching) return <Loader title="It's coming..." />;
+
+  if (error) return <Error />;
 
   return (
     <div className="flex flex-col">
@@ -31,7 +33,7 @@ const Discover = () => {
         </select>
       </div>
       <div className="flex flex-wrap sm: justify-start justify-center gap-8">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+        {data?.map((song, i) => (
           <SongCard key={song.key} song={song} i={i} />
         ))}
       </div>
